c3c94ef91e917f1ca052e36e7719baa89b3a7839
馬場悟 <babasatoru@bstr.local>
2020-08-30 21:25:04 +0900

    ちょい変更

--- db/migrate/20200225102743_create_films.rb
+++ db/migrate/20200225102743_create_films.rb
@@ -1,7 +1,7 @@
 class CreateFilms < ActiveRecord::Migration[6.0]
   def change
     create_table :films do |t|
-      t.text :body
+      t.text :name
       t.string :title
       t.string :actor
       t.integer :released_year
